d48ec03865da49bf8aa0d70c3789c7436639f532
Dray Lacy <dray@envylabs.com>
2019-04-05 18:31:23 -0400

    ci: pare down the test matrix a bit
    This addon has nothing to do with jQuery, so there's no point in having a scenario for jQuery
    integration. The 2.8 LTS tests also don't need the jquery-integration optional feature, either.

--- config/ember-try.js
+++ config/ember-try.js
@@ -13,12 +13,8 @@ module.exports = function() {
       scenarios: [
         {
           name: 'ember-lts-2.18',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({ 'jquery-integration': true })
-          },
           npm: {
             devDependencies: {
-              '@ember/jquery': '^0.5.1',
               'ember-source': '~2.18.0'
             }
           }
@@ -63,19 +59,6 @@ module.exports = function() {
           name: 'ember-default',
           npm: {
             devDependencies: {}
-          }
-        },
-        {
-          name: 'ember-default-with-jquery',
-          env: {
-            EMBER_OPTIONAL_FEATURES: JSON.stringify({
-              'jquery-integration': true
-            })
-          },
-          npm: {
-            devDependencies: {
-              '@ember/jquery': '^0.5.1'
-            }
           }
         }
       ]
